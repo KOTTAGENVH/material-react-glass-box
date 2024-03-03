03915697133de249a36a6e7c09eb6b352797051b
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-03-03 16:43:35 +0530

    Commit message describing your changes

--- GlassBoxUI.js
+++ GlassBoxUI.js
@@ -3,8 +3,10 @@ import Box from '@mui/material/Box';
 import { styled } from '@mui/system';
 
 const GlassBox = styled(Box)({
-  background: 'rgba(255, 255, 255, 0.5)',
-  backdropFilter: 'blur(10px)',
+  backdropFilter: "blur(10px)",
+  background: "rgba(255, 255, 255, 0.1)",
+  boxShadow:
+    "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.05)",
   borderRadius: '20px',
   padding: '20px',
 });
