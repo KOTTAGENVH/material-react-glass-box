2903a00d0d7c5cbe1684982a678e639d3526fca3
Kottage N.V.H <87430226+KOTTAGENVH@users.noreply.github.com>
2024-03-03 16:48:29 +0530

    Commit message describing your changes

--- GlassBoxUI.js
+++ GlassBoxUI.js
@@ -7,8 +7,6 @@ const GlassBox = styled(Box)({
   background: "rgba(255, 255, 255, 0.1)",
   boxShadow:
     "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.05)",
-  borderRadius: '20px',
-  padding: '20px',
 });
 
 function GlassBoxUI({ children, ...props }) {
